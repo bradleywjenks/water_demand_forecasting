d88f13709ee3d1a8a3345dc2c1ccf6d5b29acd13
bradleywjenks <b.jenks21@imperial.ac.uk>
2023-12-05 16:42:02 +0000

    made training period a tunable parameter

--- iai_code.jl
+++ iai_code.jl
@@ -24,7 +24,7 @@ end
 # Plot time series data
 begin
 	data_type = "inflow" # "inflow", "weather"
-	col_names = [:dma_h] # [:dma_a]
+	col_names = [:dma_c] # [:dma_a]
 	y_label = "Inflow [L/s]"
 	start_date = DateTime("2022-01-01")
 	end_date = DateTime("2022-07-23")
@@ -93,7 +93,7 @@ end
 
 # Create master dataframe from imputed datasets
 begin
-	dma_id = :dma_h
+	dma_id = :dma_d
 	lag_times = (1, 24, 168)
 	master_df = make_dataframe(X_inflow, X_weather, lag_times, dma_id)
     master_df = dropmissing(master_df) # missing data created by lag feature values
@@ -106,21 +106,24 @@ Here, we split train/test datasets for three different models: 1h, 24h, and 168h
 
 begin
 
+    n_week_train = 10
+    n_train = (1+n_week_train) * 168
+
     # 1h model datasets
-    X_1h_train = master_df[1:end-168, 3:end]
-    y_1h_train = master_df[1:end-168, 2]
+    X_1h_train = master_df[end-n_train:end-168, 3:end]
+    y_1h_train = master_df[end-n_train:end-168, 2]
     X_1h_test = master_df[end-167:end-167, 3:end]
     y_1h_test = master_df[end-167:end-167, 2]
 
     # 24h model datasets
-    X_24h_train = master_df[1:end-168, [3:end-3; end-1:end]]
-    y_24h_train = master_df[1:end-168, 2]
+    X_24h_train = master_df[end-n_train:end-168, [3:end-3; end-1:end]]
+    y_24h_train = master_df[end-n_train:end-168, 2]
     X_24h_test = master_df[end-167:end-144, [3:end-3; end-1:end]]
     y_24h_test = master_df[end-167:end-144, 2]
 
     # 24h model datasets
-    X_168h_train = master_df[1:end-168, [3:end-3; end]]
-    y_168h_train = master_df[1:end-168, 2]
+    X_168h_train = master_df[end-n_train:end-168, [3:end-3; end]]
+    y_168h_train = master_df[end-n_train:end-168, 2]
     X_168h_test = master_df[end-167:end, [3:end-3; end]]
     y_168h_test = master_df[end-167:end, 2]
 
@@ -211,11 +214,11 @@ begin
 
     # metric no. 2: max absolute error over first day of predicted week
     maxAE_1h_24h = maximum(abs.(y_24h_test .- vcat(y_1h_predict, y_24h_predict[2:end])))
-    println("Maximum absolute error of first 24h period using 1h + 24h models: $maxAE_1h_24h L/s")
+    println("MaxAE of first 24h period using 1h + 24h models: $maxAE_1h_24h L/s")
     maxAE_24h = maximum(abs.(y_24h_test .- y_24h_predict))
-    println("Maximum absolute error of first 24h period using 24h model: $maxAE_24h L/s")
+    println("MaxAE of first 24h period using 24h model: $maxAE_24h L/s")
     maxAE_168h = maximum(abs.(y_168h_test[1:24] .- y_168h_predict[1:24]))
-    println("Maximum absolute error of first 24h period using 168h model: $maxAE_168h L/s")
+    println("MaxAE of first 24h period using 168h model: $maxAE_168h L/s")
 
     # metric no. 3: mean absolute error (MAE) over first 24-h period
     mae_last_168h = (1/144) * sum(abs.(y_168h_test[25:168] .- y_168h_predict[25:168]))
@@ -228,7 +231,7 @@ end
 begin
 
     # plotting data
-    metrics = repeat(["MAE (first 24h)", "maxAE (first 24h)", "MAE (last 144h)"], inner=3)
+    metrics = repeat(["MAE (first 24h)", "MaxAE (first 24h)", "MAE (last 144h)"], inner=3)
     vals = [float.(mae_first_1h_24h), float.(mae_first_24h), float.(mae_first_168h), float.(maxAE_1h_24h), float.(maxAE_24h), float.(maxAE_168h), NaN, NaN, float.(mae_last_168h)]
     model = repeat(["1h + 24h models", "24h model", "168h model"], outer=3)
 
@@ -244,7 +247,7 @@ begin
     # plot inputs
     colors = theme_palette(:auto).colors.colors
 	y_label = "Inflow [L/s]"
-    no_prev_steps = 3 * 168 # 3 weeks of previous inflow data
+    no_prev_steps = 2 * 168 # 3 weeks of previous inflow data
 
     prev_df = master_df[end-167-no_prev_steps:end, [:date_time, :dma_inflow]]
     actual_df = DataFrame(:date_time => master_df[end-167:end, :date_time], :dma_inflow => y_168h_test)
@@ -258,6 +261,7 @@ begin
     @df predict_24h_df plot!(:date_time, :dma_inflow, color=colors[3], linewidth=1.5, label="Predicted (24h model)")
     @df predict_168h_df plot!(:date_time, :dma_inflow, color=colors[1], linewidth=1.5, label="Predicted (168h model)")
     vspan!([actual_df[1, :date_time], actual_df[end, :date_time]], color=:gray, alpha=0.2, label="Prediction window")
+
 end
 
 
